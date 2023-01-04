778da64b743c74e4a244e20c9184830b2646a69b
jamil <jamilmaqdis@gmail.com>
2023-01-04 09:58:18 +0100

    Formatting

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-export {default as DonutDoubleChart} from './BChart/DonutDoubleChart'
-export {default as StackedAreaChart} from './BChart/StackedAreaTimeSeriesChart'
-export {default as StackedBarTimeSeriesChart} from './BChart/StackedBarTimeSeriesChart'
+export { default as DonutDoubleChart } from './BChart/DonutDoubleChart'
+export { default as StackedAreaChart } from './BChart/StackedAreaTimeSeriesChart'
+export { default as StackedBarTimeSeriesChart } from './BChart/StackedBarTimeSeriesChart'
